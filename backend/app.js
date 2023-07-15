fe0bda054239f5357a87b26159232e7d7f7a7b97
Aleksandra Kirillova <kiri2lova@gmail.com>
2023-07-15 23:40:39 +0400

    fix: mongodb connection

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const app = express();
 const port = 3000;
 
 // подключаемся к БД
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 // подключаем cors middleware
 app.use(cors({
